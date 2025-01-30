b4a72986c2a211a7ef139f92da246cf4a288e719
Tsykunov Oleksandr <50213824+AlexThukynov1@users.noreply.github.com>
2025-01-30 15:28:01 +0200

    Add files via upload

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,7 +1,22 @@
-const TeacherPage = () => {
+import Announcements from "@/components/Announcements"
+import EventCalendar from "@/components/EventCalendar"
+import MainCalendar from "@/components/MainCalendar"
+
+const TeachertPage = () => {
   return (
-    <div className=''>TeacherPage</div>
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      <div className="w-full lg:w-2/3 flex flex-col gap-4">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <MainCalendar/>
+        </div>
+      </div>
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar/>
+        <Announcements/>
+      </div>
+    </div>
   )
 }
 
-export default TeacherPage
+export default TeachertPage
